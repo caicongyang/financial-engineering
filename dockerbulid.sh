08de23218f541653b1b6d638744258e5cab34d7e
caicongyang <caicongyang@gmail.com>
2025-03-04 16:42:07 +0800

    add some code

--- dockerbulid.sh
+++ dockerbulid.sh
@@ -2,12 +2,12 @@
 #!/bin/bash
 
 # 构建镜像
-docker build -t financial-data-processor:v1 .
+docker build -t financial-data-processor:latest .
 
 # 打标签
-docker tag financial-data-processor:v1 caicongyang/financial-data-processor:v1
+docker tag financial-data-processor:latest caicongyang/financial-data-processor:latest
 
 # 推送镜像
-docker push caicongyang/financial-data-processor:v1
+docker push caicongyang/financial-data-processor:latest
 
 
